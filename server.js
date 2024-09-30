1d05910cefe4def7a047505b48714e9e7c2770ab
rajthanusan <rajthanusan08@gmail.com>
2024-09-30 16:37:29 +0530

    errors removed

--- server.js
+++ server.js
@@ -907,19 +907,19 @@ app.put("/api/LeaveApply/:id/:action", (req, res) => {
           }
 
           const employeeEmail = results[0].email; // Assuming username is the email
-          const subject = `Leave Request ${
-            action.charAt(0).toUpperCase() + action.slice(1)
-          }`;
+          const subject = `Leave Request ${action.charAt(0).toUpperCase() + action.slice(1)}`;
+
           const text = `Dear User,
-
+          
           We would like to inform you that the status of your leave request has been updated.
-
+          
           Your leave request has been **${action}**. You can check the status by using the following link: https://lms-model.netlify.app/myleave.
-
+          
           If you have any questions or need further assistance, please do not hesitate to contact us.
-
+          
           Thank you,  
           The Leave Management System Team`;
+          
 
           // Send email
           try {
